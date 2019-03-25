a064cd193a20e30e21f7b3cd90047295a30fb2ff
moriaMaman <44782836+moriaMaman@users.noreply.github.com>
2019-03-25 16:13:41 +0200

    Add files via upload

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <stdlib.h>  
 using std::cout, std::endl;
 #include "badkan.hpp"
 #include "Tree.hpp"
@@ -16,16 +17,17 @@ int main() {
   threetree.insert(5);
   threetree.insert(7);
   threetree.insert(3);
+
   ariel::Tree exampletree;
-  threetree.insert(3);
-  threetree.insert(7);
-  threetree.insert(9);
-  threetree.insert(11);
-  threetree.insert(1);
-  threetree.insert(8);
-  threetree.insert(2);
-  threetree.insert(5);
-  threetree.insert(6);   
+  exampletree.insert(3);
+  exampletree.insert(7);
+  exampletree.insert(9);
+  exampletree.insert(11);
+  exampletree.insert(1);
+  exampletree.insert(8);
+  exampletree.insert(2);
+  exampletree.insert(5);
+  exampletree.insert(6);   
   
   ariel::Tree mytree;  
 
@@ -50,29 +52,52 @@ int main() {
   .CHECK_THROWS(threetree.left(6))
   .CHECK_OK    (threetree.print())
 
-  .CHECK_EQUAL (threetree.size(), 9)
-  .CHECK_EQUAL (threetree.root(), 3)
-  .CHECK_EQUAL (threetree.parent(9), 7)
-  .CHECK_EQUAL (threetree.parent(2), 1)
-  .CHECK_EQUAL (threetree.parent(threetree.parent(2)), 3)
-  .CHECK_EQUAL (threetree.left(9), 8)
-  .CHECK_THROWS(threetree.insert(14))
-  .CHECK_THROWS(threetree.insert(4))
-  .CHECK_EQUAL (threetree.size(), 11)
-  .CHECK_OK    (emptytree.remove(5))
-  .CHECK_EQUAL (emptytree.contains(5), false)
-  .CHECK_EQUAL (threetree.size(), 10)
-  .CHECK_OK    (emptytree.remove(3))
-  .CHECK_EQUAL (threetree.root(), 2)
-  .CHECK_EQUAL (threetree.right(5), 6)
-  .CHECK_EQUAL (emptytree.contains(3), false)
-  .CHECK_EQUAL (emptytree.contains(2), true)
-  .CHECK_THROWS(threetree.insert(12))
-  .CHECK_THROWS(threetree.insert(3))
-  .CHECK_EQUAL (threetree.size(), 11)
-  .CHECK_EQUAL (threetree.right(threetree.right(7)), 11)
-  .CHECK_OK    (threetree.print())
-  .print();
+  .CHECK_EQUAL (exampletree.size(), 9)
+  .CHECK_EQUAL (exampletree.root(), 3)
+  .CHECK_EQUAL (exampletree.parent(9), 7)
+  .CHECK_EQUAL (exampletree.parent(2), 1)
+  .CHECK_EQUAL (exampletree.parent(threetree.parent(2)), 3)
+  .CHECK_EQUAL (exampletree.left(9), 8)
+  .CHECK_THROWS(exampletree.insert(14))
+  .CHECK_THROWS(exampletree.insert(4))
+  .CHECK_EQUAL (exampletree.size(), 11)
+  .CHECK_OK    (exampletree.remove(5))
+  .CHECK_EQUAL (exampletree.contains(5), false)
+  .CHECK_EQUAL (exampletree.size(), 10)
+  .CHECK_OK    (exampletree.remove(3))
+  .CHECK_EQUAL (exampletree.root(), 2)
+  .CHECK_EQUAL (exampletree.right(5), 6)
+  .CHECK_EQUAL (exampletree.contains(3), false)
+  .CHECK_EQUAL (exampletree.contains(2), true)
+  .CHECK_THROWS(exampletree.insert(12))
+  .CHECK_THROWS(exampletree.insert(3))
+  .CHECK_EQUAL (exampletree.size(), 11)
+  .CHECK_EQUAL (exampletree.right(threetree.right(7)), 11)
+  .CHECK_EQUAL (exampletree.parent(12), 14)
+  .CHECK_EQUAL (exampletree.parent(9), 8)
+  .CHECK_EQUAL (exampletree.left(7), 4)
+  .CHECK_EQUAL (exampletree.left(2), 1)
+  .CHECK_EQUAL (exampletree.right(4), 6)
+  .CHECK_EQUAL (exampletree.right(9), 11)
+   .print();
+ 
+  for (int i=0; i<=100; i++){
+     int temp = rand() % 100; 
+     if(mytree.contains(temp)==false){
+       mytree.insert(temp);
+       tc.CHECK_EQUAL (mytree.contains(temp), true);
+       mytree.remove(temp);
+      tc.CHECK_EQUAL (mytree.contains(temp), false);
+     }
+     else{
+          mytree.remove(temp);
+         tc.CHECK_EQUAL (mytree.contains(temp), false);
+     }
+   
+  }
+  tc.CHECK_EQUAL (mytree.size(), 0);
   
+ 
+ tc.print();  
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
 }
